7180054a47a3877e2fd616959583858c0c590d60
Chris Sedlmayr <chris@sedlmayr.co.uk>
2012-11-12 20:19:40 +0000

    Now supporting folder of pictures

--- web/index.php
+++ web/index.php
@@ -24,32 +24,37 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->get('/', function () use ($app, $key, $secret) {
 
     // temp pictures array until I can read it from S3...
-    $pictures = array(
-        array('filename' => '047.JPG'),
-        array('filename' => '052.JPG'),
-        array('filename' => '064.JPG'),
-        array('filename' => '076.JPG'),
-        array('filename' => '082.JPG'),
-        array('filename' => '090.JPG'),
-        array('filename' => '096.JPG'),
-        array('filename' => '100.JPG'),
-        array('filename' => '105.JPG'),
-        array('filename' => '107.JPG'),
-        array('filename' => '112.JPG'),
-        array('filename' => '117.JPG'),
-        array('filename' => '120.JPG'),
-        array('filename' => '125.JPG'),
-        array('filename' => '126.JPG'),
-        array('filename' => '133.JPG'),
-        array('filename' => '147.JPG'),
-        array('filename' => 'P1020836.JPG'),
-        array('filename' => 'P1020841.JPG'),
-        array('filename' => 'P1020844.JPG'),
-        array('filename' => 'P1020858.JPG'),
-        array('filename' => 'P1020861.JPG'),
-        array('filename' => 'P1020863.JPG'),
-        array('filename' => 'P1020869.JPG'),
-        array('filename' => 'P1020871.JPG')
+    $folders = array(
+        'My First Bath' => array(
+            array('filename' => 'P1020923.JPG')
+        ),
+        'Other' => array(
+            array('filename' => '047.JPG'),
+            array('filename' => '052.JPG'),
+            array('filename' => '064.JPG'),
+            array('filename' => '076.JPG'),
+            array('filename' => '082.JPG'),
+            array('filename' => '090.JPG'),
+            array('filename' => '096.JPG'),
+            array('filename' => '100.JPG'),
+            array('filename' => '105.JPG'),
+            array('filename' => '107.JPG'),
+            array('filename' => '112.JPG'),
+            array('filename' => '117.JPG'),
+            array('filename' => '120.JPG'),
+            array('filename' => '125.JPG'),
+            array('filename' => '126.JPG'),
+            array('filename' => '133.JPG'),
+            array('filename' => '147.JPG'),
+            array('filename' => 'P1020836.JPG'),
+            array('filename' => 'P1020841.JPG'),
+            array('filename' => 'P1020844.JPG'),
+            array('filename' => 'P1020858.JPG'),
+            array('filename' => 'P1020861.JPG'),
+            array('filename' => 'P1020863.JPG'),
+            array('filename' => 'P1020869.JPG'),
+            array('filename' => 'P1020871.JPG')
+        )
     );
 
 
@@ -62,7 +67,7 @@ $app->get('/', function () use ($app, $key, $secret) {
 
     return $app['twig']->render('homepage.twig', array(
         'files' => $files,
-        'pictures'  => $pictures
+        'folders'  => $folders
     ));
 })->bind('homepage');
 
